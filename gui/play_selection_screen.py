180e3072edae17b2dd792ba97a1630cdff1ee917
BilyiPJATK <s31718@pjwstk.edu.pl>
2025-05-28 10:04:30 +0200

    removed category button from play_selection_screen.py

--- gui/play_selection_screen.py
+++ gui/play_selection_screen.py
@@ -21,9 +21,6 @@ class PlaySelectionScreen(ttk.Frame):  # inherit from ttk.Frame
         btn_custom = ttk.Button(self, text="Custom Word", width=20, command=lambda: (play_sound(), self.play_custom()), style="TButton")
         btn_custom.pack(pady=10)
 
-        btn_category = ttk.Button(self, text="Category", width=20, command=lambda: (play_sound(), self.play_category()), style="TButton")
-        btn_category.pack(pady=10)
-
         btn_back = ttk.Button(self, text="Back to Menu", command=lambda: (play_sound(), controller.show_frame("MainMenu")), style="TButton")
         btn_back.pack(pady=20)
 
@@ -66,8 +63,6 @@ class PlaySelectionScreen(ttk.Frame):  # inherit from ttk.Frame
         btn_back = ttk.Button(popup, text="Back", command=lambda: (play_sound(), cancel()), style="TButton")
         btn_back.pack(side="right", padx=20, pady=20)
 
-    def play_category(self):
-        messagebox.showinfo("Category Mode", "Category mode selected. (To be implemented)")
 
 def play_sound():
     play_sound_effect("assets/sfx/click.wav")
